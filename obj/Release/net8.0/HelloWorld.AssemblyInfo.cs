cfc3c52d92ebf96af12109c709085acaa8c15396
giyanwei <2061627424@qq.com>
2024-05-25 21:35:18 +0800

    Release v1.0

--- obj/Release/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Release/net8.0/HelloWorld.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68130e732365d08e17ed31e5f0ae780ccfe3b04e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
